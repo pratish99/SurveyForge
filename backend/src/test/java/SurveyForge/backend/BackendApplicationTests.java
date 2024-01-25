8cac600cf297facbbfc2024d224b1c2a21478679
Pratish Soni <sonipratish32@gmail.com>
2024-01-25 16:57:34 +0530

    initial commit

---
+++ backend/src/test/java/SurveyForge/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SurveyForge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
