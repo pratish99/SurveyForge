12c47ba8d92ae5f970e1aa9d88a5af472dfd12c0
yashrathee10 <yash.rathee13@gmail.com>
2024-01-29 10:51:18 +0530

    Answer Changes

--- backend/src/main/java/SurveyForge/backend/Services/Implementation/SurveyServiceImpl.java
+++ backend/src/main/java/SurveyForge/backend/Services/Implementation/SurveyServiceImpl.java
@@ -1,13 +1,17 @@
 package SurveyForge.backend.Services.Implementation;
 
 import SurveyForge.backend.Entities.Survey;
+import SurveyForge.backend.Models.SurveyModel;
+import SurveyForge.backend.Responses.Response;
 import SurveyForge.backend.Services.SurveyService;
 import org.springframework.http.ResponseEntity;
 
 public class SurveyServiceImpl implements SurveyService {
 
+
     @Override
-    public ResponseEntity createSurvey(Survey survey) {
+    public Response createSurvey(SurveyModel surveyModel) {
+
         return null;
     }
 }
